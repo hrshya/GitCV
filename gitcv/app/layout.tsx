ed335d23ec0d9142fa95f02734b95a1a5177303e
hrshya <harshyadav6078@gmail.com>
2026-06-15 17:02:59 +0530

    chore: add speed insights

--- gitcv/app/layout.tsx
+++ gitcv/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
